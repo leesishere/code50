da3e07cf083bdd5edc02c184346ee271aa1a89ba
leesishere <leesishere@gmail.com>
2024-11-05 20:57:15 +0000

    Tue, Nov 5, 2024, 3:57 PM -05:00

---
+++ deep/deep.py
@@ -0,0 +1,10 @@
+###################
+# In deep.py, implement a program that prompts the user for the answer to the
+# Great Question of Life, the Universe and Everything,
+# outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+###################
+
+answer = ["42","orty-two","forty two"]
+
+inpuy_answer = input("What is the answer to the Great Question of Life, the Universe and Everything?")
+
