08900b1f6c326ccbe385a427dc94272b5a1c0e4f
leesishere <leesishere@gmail.com>
2024-11-05 17:37:08 +0000

    Tue, Nov 5, 2024, 12:37 PM -05:00

--- faces/faces.py
+++ faces/faces.py
@@ -32,8 +32,6 @@ unicode_dict = {
 }
 
 
-
-
 def main():
     message = input()
     response = emoticons(message)
@@ -47,8 +45,8 @@ def emoticons(s):
 
     # One-liner to find the key for the given value
     key_for_value = next(key for key, value in unicode_dict.items() if value == last_element)
-    return_message = ", ".join(my_list[:-1])
-    return return_message + make_printable(key_for_value)
+    return_message = ", ".join(split_list[:-1])
+    return return_message + " " + make_printable(key_for_value)
 
 def make_printable(u):
        # Remove the 'U+' prefix
