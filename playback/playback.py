8eb41da0b66dce918fb4da276e3ef97d4c3dce83
leesishere <leesishere@gmail.com>
2024-11-05 16:10:06 +0000

    Tue, Nov 5, 2024, 11:10 AM -05:00

--- playback/playback.py
+++ playback/playback.py
@@ -3,13 +3,7 @@ def main():
     print(slow(input()))
 
 def slow(s):
-    # Split the string by spaces
-    split_text = s.split(' ')
-
-    # Replace ' ' with '...' in the list
-    replaced_text = [word + '...' for word in split_text]
-
-    # Join the list back into a string
-    return '...'.join(replaced_text)
+   # slit space and then Join the list back into a string...
+    return '...'.join(s.split(' '))
 
 main()
