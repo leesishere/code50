e556f2908acdc10c82e1aa2e65d44a5bf8ea542e
leesishere <leesishere@gmail.com>
2024-11-05 20:02:40 +0000

    Tue, Nov 5, 2024, 3:02 PM -05:00

--- tip/tip.py
+++ tip/tip.py
@@ -4,14 +4,25 @@ def main():
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
+####################################
+#  dollars_to_float, which should accept a str as input (formatted as $##.##, wherein each # is a decimal digit),
+#  remove the leading $, and return the amount as a float. For instance, given $50.00 as input, it should return 50.0.
+####################################
+def dollars_to_float(d: str) -> float:
+    # Remove the dollar sign
+    d = d.replace("$", "")
+    # Convert the string to a float
+    return float(d)
 
-def dollars_to_float(d):
-    # dollars_to_float, which should accept a str as input (formatted as $##.##, wherein each # is a decimal digit),
-    # remove the leading $, and return the amount as a float. For instance, given $50.00 as input, it should return 50.0.
-
-
-def percent_to_float(p):
-    # TODO
-
+####################################
+# percent_to_float, which should accept a str as input (formatted as ##%, wherein each # is a decimal digit),
+# remove the trailing %, and return the percentage as a float. For instance, given 15% as input, it should return 0.15.
+####################################
+def percent_to_float(p: str) -> float:
+    # Remove the % sign
+    p = p.replace("%", "")
+    # Convert the string to a float
+    float(d)
+    return float(d)
 
 main()
