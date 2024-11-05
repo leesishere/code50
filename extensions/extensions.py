3d854eb62d300d4effca4e66669186ccb686f523
leesishere <leesishere@gmail.com>
2024-11-05 23:19:06 +0000

    Tue, Nov 5, 2024, 6:19 PM -05:00

---
+++ extensions/extensions.py
@@ -0,0 +1,9 @@
+extension = {
+    ".gif", "image/gif",
+    ".jpg", "image/jpeg",
+    ".jpeg", "image/jpeg",
+    ".png", "image/png",
+    ".pdf", "application/pdf".
+    ".txt", "text/plain",
+    ".zip", "application/zip"
+}
