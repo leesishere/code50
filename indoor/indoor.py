efc98daa3e0f9857352a5d3613a1f3baac233fac
leesishere <leesishere@gmail.com>
2024-11-05 02:03:51 +0000

    Mon, Nov 4, 2024, 9:03 PM -05:00

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,8 +1,12 @@
 print(input().lower())
-#print(first)
+print(input().lower())
+print(input().lower())
 
+
+'''
 second = input().lower()
 print(second)
 
 three = input().lower()
 print(three)
+'''
