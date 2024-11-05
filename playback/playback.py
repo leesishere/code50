9c9218258d61d084ff0d3c0e4eb0b2093b050e36
leesishere <leesishere@gmail.com>
2024-11-05 16:02:01 +0000

    Tue, Nov 5, 2024, 11:02 AM -05:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,16 +1,21 @@
 
+def main():
+
+
+def slow(s):
+    # Split the string by spaces
+    split_text = s.split(' ')
+
+    # Replace ' ' with '...' in the list
+    replaced_text = [word.replace('CS50', 'CS50x') for word in split_text]
+
+    # Join the list back into a string
+    result = ' '.join(replaced_text)
 
 # Original string
 text = "Hello, welcome to CS50. CS50 is great!"
 
-# Split the string by spaces
-split_text = text.split()
 
-# Replace 'CS50' with 'CS50x' in the list
-replaced_text = [word.replace('CS50', 'CS50x') for word in split_text]
-
-# Join the list back into a string
-result = ' '.join(replaced_text)
 
 print(result)
 
