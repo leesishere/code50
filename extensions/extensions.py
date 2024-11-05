085c2c0778618807805c51fd34f5bc6251976c90
leesishere <leesishere@gmail.com>
2024-11-05 23:32:33 +0000

    Tue, Nov 5, 2024, 6:32 PM -05:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,11 +1,11 @@
 extension = {
-    "gif", "image/gif",
-    "jpg", "image/jpeg",
-    "jpeg", "image/jpeg",
-    "png", "image/png",
-    "pdf", "application/pdf",
-    "txt", "text/plain",
-    "zip", "application/zip"
+    "gif":"image/gif",
+    "jpg":"image/jpeg",
+    "jpeg":"image/jpeg",
+    "png":"image/png",
+    "pdf":"application/pdf",
+    "txt":"text/plain",
+    "zip":"application/zip"
 }
 
 file, ext = input("File name: ").split(".")
