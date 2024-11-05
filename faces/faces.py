e804bb75294f3c57d7a70cefdf4c57e6f296242c
leesishere <leesishere@gmail.com>
2024-11-05 16:32:11 +0000

    Tue, Nov 5, 2024, 11:32 AM -05:00

--- faces/faces.py
+++ faces/faces.py
@@ -2,7 +2,8 @@ def main():
     #print(slow(input()))
 
 def emoticons(s):
-    s.plit(' ')
+    find_emotions = s.plit(' ')
+    [x for x in find_emotions if x == ':) ]
    # slit space and then Join the list back into a string...
     return '...'.join(s.split(' '))
 
