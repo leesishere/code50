e6b689f3eff866788460b8c5dadd3d9e15b8a001
leesishere <leesishere@gmail.com>
2024-11-05 21:00:57 +0000

    Tue, Nov 5, 2024, 4:00 PM -05:00

--- deep/deep.py
+++ deep/deep.py
@@ -4,9 +4,11 @@
 # outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
 ###################
 
-answer = ["42","orty-two","forty two"]
+answer = ["42","forty-two","forty two"]
 
-input_answer = input("What is the answer to the Great Question of Life, the Universe and Everything?")
+input_answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
 
 if input_answer in answer:
     print("Yes")
+else:
+    print("No")
