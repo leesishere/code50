04117ee1929ea493452b662163edf984d9700ebf
leesishere <leesishere@gmail.com>
2024-11-06 00:58:00 +0000

    Tue, Nov 5, 2024, 7:58 PM -05:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -2,7 +2,7 @@ def main():
     x, y, z = input("Expression: ").split(' ')
     print(math_it(int(x),y,int(z)))
 
-def math_it(x,y,z):
+def math_it(x,y,z) -> float:
     match y:
         case '+':
             return x + z
