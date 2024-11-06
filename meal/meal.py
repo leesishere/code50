85e87176aafb27ae77cc30cfee4a8b550a1b0b01
leesishere <leesishere@gmail.com>
2024-11-06 03:00:25 +0000

    Tue, Nov 5, 2024, 10:00 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -24,7 +24,7 @@ def convert(time):
         min = min.replace('p.m.', '').strip()
         min = int(min)
         hour = int(hour) + 12
-    return hour, min
+    return int(hour), int(min)
 
 if __name__ == "__main__":
     main()
