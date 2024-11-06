22637495bfd63403789fbd9bc3738454e1e618ab
leesishere <leesishere@gmail.com>
2024-11-06 01:55:24 +0000

    Tue, Nov 5, 2024, 8:55 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,3 +1,9 @@
+######################
+# Suppose that you’re in a country where it’s customary to eat breakfast between 7:00 and 8:00, lunch between 12:00 and 13:00,
+# and dinner between 18:00 and 19:00. Wouldn’t it be nice if you had a program that could tell you what to eat when?
+######################
+
+
 def main():
     ...
 
@@ -9,7 +15,7 @@ def convert(time):
 if __name__ == "__main__":
     main()
 
-    
+
 
 breakfast time
 lunch time
