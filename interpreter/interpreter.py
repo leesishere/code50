6714181424d309a94463a69575d79c7040daccdb
leesishere <leesishere@gmail.com>
2024-11-06 00:52:50 +0000

    Tue, Nov 5, 2024, 7:52 PM -05:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,16 +1,15 @@
 x, y, z = input("Expression: ").split(' ')
 
 
-def classify_math(x,y,z):
-    match y:
-        case '+':
-            return x + z
-        case '-':
-            return x - z
-        case '*':
-            return x * z
-        case '/':
-            return x / z
-        case _:
-            return "Other number"
+match y:
+    case '+':
+        return x + z
+    case '-':
+        return x - z
+    case '*':
+        return x * z
+    case '/':
+        return x / z
+    case _:
+        return "Other number"
 
