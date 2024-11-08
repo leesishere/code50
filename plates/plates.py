0eb7cd1e5c737c5b1e466a7a03314c72930f6ffc
leesishere <leesishere@gmail.com>
2024-11-08 20:49:35 +0000

    Fri, Nov 8, 2024, 3:49 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,6 +1,5 @@
-# “All vanity plates must start with at least two letters.”
-# “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”#
 # “Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
+
 # “No periods, spaces, or punctuation marks are allowed.”
 import re
 
@@ -13,9 +12,9 @@ def main():
         print("Invalid")
 
 def is_valid(s):
-    if len(s) < 2 or len(s) > 6:
-        return False
-    if count_letters(s[:2]) != 2:
+    # “All vanity plates must start with at least two letters.”
+    # “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”#
+    if count_letters(s[:2]) != 2 or len(s) > 6:
         return False
     if count_numbers:
         plate_list = order_of_things(s)
