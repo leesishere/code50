286eaca6f643b3be044425afe6b7d16c605ed11e
leesishere <leesishere@gmail.com>
2024-11-08 16:15:40 +0000

    Fri, Nov 8, 2024, 11:15 AM -05:00

---
+++ coke/coke.py
@@ -0,0 +1,6 @@
+# Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins
+# in these denominations: 25 cents, 10 cents, and 5 cents.
+#
+# Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
+# Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
+
