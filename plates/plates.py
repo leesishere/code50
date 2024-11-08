257ba85085689fb0b8ba5afac4234e2734863d35
leesishere <leesishere@gmail.com>
2024-11-08 23:58:42 +0000

    Fri, Nov 8, 2024, 6:58 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -18,7 +18,7 @@ def is_valid(s):
         # Numbers cannot be used in the middle of a plate; they must come at the end.
         # For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
         # CS50P2
-        order_of_things
+        order_of_things(s)
         if count_numbers(plate_list[-1]):
             return False
 
