028ff81d5c6f52372f60934754a14ed863dfc067
leesishere <leesishere@gmail.com>
2024-11-08 19:16:58 +0000

    Fri, Nov 8, 2024, 2:16 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -2,3 +2,7 @@
 # “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”#
 # “Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
 # “No periods, spaces, or punctuation marks are allowed.”
+
+
+letters = r'[a-zA-Z]'
+pattern = r'(\d+|\D+)'
