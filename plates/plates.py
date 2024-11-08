07ffbc8bc8563b4cb4e47ea3f3c33e5f0f2efaef
leesishere <leesishere@gmail.com>
2024-11-08 21:00:35 +0000

    Fri, Nov 8, 2024, 4:00 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -22,7 +22,8 @@ def is_valid(s):
         if count_letters(plate_list[0]) and count_letters(plate_list[-1]):
             return False
         #The first number used cannot be a ‘0’.”
-        [1 for num in s if num in numbers]
+        [num for num in plate_list if num in numbers]
+
 def count_letters(s):
     alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     return sum(1 for char in s if char.upper() in alphabet)
