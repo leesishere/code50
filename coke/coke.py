aa0c3bf24c42801d9f69bd6cda98f56876860553
leesishere <leesishere@gmail.com>
2024-11-08 18:51:56 +0000

    Fri, Nov 8, 2024, 1:51 PM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,27 +3,22 @@
 #
 # Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
 # Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
-import re
 
-text = "abc123def456ghi789..."
-
+# accepted cents
+cents = [5,10,25]
 
 def main():
     amount = 50
     while amount > 0:
+        print(f"Amount Due: {amount}")
         try:
             coin = input("Insert Coin: ")
             coin = int(coin)
         except:
             # Define the pattern to split by digits and non-digits
-            #pattern = r'(\d+|\D+)'
-            #parts = re.findall(pattern, coin)
-            #amount -= int(parts[0])
-            #print(f"Amount Due: {amount}{parts[1]}")
             continue
         amount -= coin
-        if amount > 0:
-            print(f"Amount Due: {amount}")
+
 
     print("Change Owed:", abs(amount))
 
