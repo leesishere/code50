1beffd70cf3d92b5bf39dc195df895350c91a357
leesishere <leesishere@gmail.com>
2024-11-08 13:06:23 +0000

    Fri, Nov 8, 2024, 8:06 AM -05:00

--- camel/camel.py
+++ camel/camel.py
@@ -4,8 +4,12 @@ def main():
     snake_case = convert(camelCase)
 
 def convert(camelCase):
+    snake_case = ''
     for i in range(len(camelCase)):
-        print(camelCase[i].isupper())
+        if camelCase[i].isupper()):
+            snake_case = nake_case + '_' + camelCase[i].lower()
+        else:
+            snake_case = nake_case + camelCase[i]
 
 if __name__ == "__main__":
     main()
