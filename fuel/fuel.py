fe4ecc9993476f1703cd56f70027d77164e9762c
leesishere <leesishere@gmail.com>
2024-11-09 14:03:34 +0000

    Sat, Nov 9, 2024, 9:03 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,7 +3,10 @@ def main():
     while True:
         fract = input("Fraction: ")
         if type(fract) == type(1.0):
-            
+            percentage = fract * 100
+            print_string = f"{percentage}%"
+            break
+    print(f"{print_string}")
 
 def convert_fraction(f):
     f = f.replace(" ", "")
