c06a212c8f51a4bed6fdd6240254906f52194b8d
leesishere <leesishere@gmail.com>
2024-11-09 21:46:04 +0000

    Sat, Nov 9, 2024, 4:46 PM -05:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,12 +1,11 @@
 
 def main:
-
+    grocery_list =[ ]
 
     while True:
         try:
             item = input().lower().title().strip()
-            total += menu[item]
-            print(f"Total: ${total:.2f}")
+            
         except EOFError:
             # needed to add the final total to get the test script to pass the code :-(
             print(f"${total:.2f}")
