343b73abc6f1d6b7adb4aa81baccfc0efaf8a123
leesishere <leesishere@gmail.com>
2024-11-09 01:35:21 +0000

    Fri, Nov 8, 2024, 8:35 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -19,6 +19,7 @@ def is_valid(s):
         # For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
         # CS50P2
         print(plate_list)
+        
 
         if count_numbers(plate_list[-1]):
             return False
