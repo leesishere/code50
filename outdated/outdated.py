f22a00eb53c01d316460fa0be4c313e93d8c66f0
leesishere <leesishere@gmail.com>
2024-11-09 23:56:01 +0000

    Sat, Nov 9, 2024, 6:56 PM -05:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -2,18 +2,18 @@ import re
 
 def main():
     [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
     ]
 
     # accept September 8, 1636 or 9/8/1636 and return 1636-9-8
