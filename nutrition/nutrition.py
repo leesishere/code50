caea526bcf5b15d73ea0c8c79ef8e0f691e3312a
leesishere <leesishere@gmail.com>
2024-11-09 02:15:43 +0000

    Fri, Nov 8, 2024, 9:15 PM -05:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,22 +1,30 @@
 fruit = {
-    'Apple':130,
-    'Avocado':50,
-    'Banana':110,
-    'Cantaloupe':50,
-    'Grapefruit':60,
-    'Grapes':90,
-    'Honeydew':50,
-    'Kiwifruit':90,
-    'Lemon':15,
-    'Lime':20,
-    'Nectarine':60,
-    'Orange':80,
-    'Peach':60,
-    'Pear':100,
-    'Pineapple':50,
-    'Plums':70,
-    'Strawberries':50,
-    'Sweet Cherries':100,
-    'Tangerine':50,
-    'Watermelon':80
+    'apple':130,
+    'avocado':50,
+    'banana':110,
+    'cantaloupe':50,
+    'grapefruit':60,
+    'grapes':90,
+    'honeydew':50,
+    'kiwifruit':90,
+    'lemon':15,
+    'lime':20,
+    'nectarine':60,
+    'orange':80,
+    'peach':60,
+    'pear':100,
+    'pineapple':50,
+    'plums':70,
+    'strawberries':50,
+    'sweet cherries':100,
+    'tangerine':50,
+    'watermelon':80
 }
+
+def main():
+    item = input("Input: ")
+
+    print("Coleries:", fruit[item])
+
+if __name__ == "__main__":
+    main()
