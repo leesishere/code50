22f0756137ef0ee4778990572cb242e5d2b861c3
leesishere <leesishere@gmail.com>
2024-11-09 21:26:42 +0000

    Sat, Nov 9, 2024, 4:26 PM -05:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -18,6 +18,7 @@ def main():
             total += menu[item]
             print(f"Total: ${total:.2f}")
         except EOFError:
+            # needed to add the final total to get the test script to pass the code :-(
             print(f"${total:.2f}")
             exit()
         except Exception:
