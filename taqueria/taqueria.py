a2cc0aa60bf96e450399615e923c71d2e521d643
leesishere <leesishere@gmail.com>
2024-11-09 19:11:44 +0000

    Sat, Nov 9, 2024, 2:11 PM -05:00

---
+++ taqueria/taqueria.py
@@ -0,0 +1,18 @@
+def main():
+{
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+
+
+
+if __name__ == "__main__":
+    main()
