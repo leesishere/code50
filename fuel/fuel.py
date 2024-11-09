cc3962c7d79a53673d666883e0b830e14e2ba0eb
leesishere <leesishere@gmail.com>
2024-11-09 14:01:00 +0000

    Sat, Nov 9, 2024, 9:01 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,17 +1,17 @@
 
 def main():
-
-
+    while True:
+        fract = input("Fraction: ")
+        if type(fract) == type(1.0):
+            
 
 def convert_fraction(f):
     f = f.replace(" ", "")
-
-    while True:
-        try:
-            numerator, denominator = f.split('/')
-            return float(umerator/denominator)
-        except ValueError:
-            pass
+    try:
+        numerator, denominator = f.split('/')
+        return float(umerator/denominator)
+    except ValueError:
+        pass
 
 
 
