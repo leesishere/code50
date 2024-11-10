3f3243b9cbf71718762822fba9f9caf7fd0c66f3
leesishere <leesishere@gmail.com>
2024-11-10 15:45:09 +0000

    Sun, Nov 10, 2024, 10:45 AM -05:00

--- itunes/itunes.py
+++ itunes/itunes.py
@@ -2,5 +2,7 @@ import json
 import requests
 
 response = requests.get('https://itunes.apple.com/search?entity=song&limit=1&term=weezer')
-print(json.dumps(response.json(), indent=4))
+artist = response.json()
 
+for result in artist['results']:
+    print(result["trackName"])
