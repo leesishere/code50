8419d733c0b8d8c2dade3fdb0b87b2be323d9132
leesishere <leesishere@gmail.com>
2024-11-10 18:15:31 +0000

    Sun, Nov 10, 2024, 1:15 PM -05:00

--- itunes/itunes.py
+++ itunes/itunes.py
@@ -10,6 +10,23 @@ artist = response.json()
 for result in artist["results"]:
     print(result["trackName"])
 
+my_list = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+]
+result = some_function_that_takes_arguments(
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+)
+
 
 def cow(s):
     my_cow = r"""
