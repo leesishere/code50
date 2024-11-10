ea9bbff2ae38573f1c2fbcfaa9963eaadff75c3e
leesishere <leesishere@gmail.com>
2024-11-10 16:00:08 +0000

    Sun, Nov 10, 2024, 11:00 AM -05:00

--- itunes/itunes.py
+++ itunes/itunes.py
@@ -1,6 +1,5 @@
 import json
 import requests
-import cowsay
 
 response = requests.get('https://itunes.apple.com/search?entity=song&limit=50&term=weezer')
 artist = response.json()
@@ -8,4 +7,19 @@ artist = response.json()
 for result in artist['results']:
     print(result["trackName"])
 
-cowsay.cow('Hello World')
+
+def cow(s):
+    print('''
+  ___________
+| Hello World |
+  ===========
+           \
+            \
+              ^__^
+              (oo)\_______
+              (__)\       )\/\
+                  ||----w |
+                  ||     ||)
+''')
+
+cow('Hello World')
