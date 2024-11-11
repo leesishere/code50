4fda924ca3cfb1d8224ae576ac3d597d4382f8db
leesishere <leesishere@gmail.com>
2024-11-11 00:55:39 +0000

    Sun, Nov 10, 2024, 7:55 PM -05:00

---
+++ emojize/emojize.py
@@ -0,0 +1,13 @@
+
+def main():
+    # Example usage:
+    thumbs_up_emojis = ["🤚", "👍", "👏"]
+
+    print("Thumbs up emojis:")
+    for emoji in thumbs_up_emojis:
+        print(emoji)
+
+
+
+if __name__ == "__main__":
+    main()
