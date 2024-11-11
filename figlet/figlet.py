8cb4f2ca5b43417251f057d50d3c2b104e6f848f
leesishere <leesishere@gmail.com>
2024-11-11 15:31:19 +0000

    Mon, Nov 11, 2024, 10:31 AM -05:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,7 +6,8 @@ from pprint import pprint
 class CustomArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         self.print_usage()
-        custom_message = f"Custom error: {message}\n"
+        # custom_message = f"Custom error: {message}\n"
+        custom_message = f"Invalid usage\n"
         self.exit(2, custom_message)
 
 def main():
