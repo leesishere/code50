e960e46d3c6547a02f1db8c7cbd2bfde43ca7538
leesishere <leesishere@gmail.com>
2024-11-11 20:08:55 +0000

    Mon, Nov 11, 2024, 3:08 PM -05:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,4 +1,16 @@
+
 def main():
+    name_list = []
+    try:
+        while True:
+            user_input = input("Name: ")
+            name_list.append(user_input)
+    except EOFError:
+        print("Adieu, adieu, to ",end='')
+        for name in name_list[1:-1]:
+            print(name,end='')
+        print()
+
 
 
 if __name__ == "__main__":
