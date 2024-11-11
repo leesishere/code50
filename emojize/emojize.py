c77d2f1c9e46651a0bc3a3371c4b38a4781aa5b4
leesishere <leesishere@gmail.com>
2024-11-11 01:45:53 +0000

    Sun, Nov 10, 2024, 8:45 PM -05:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -5,8 +5,9 @@ from emoji import emoji_dict
 emoji = emoji_dict
 
 def main():
-
-    print(emoji[':1st_place_medal:'])
+    if isinstance(emoji, set):
+        print("It's a set!")
+        print(emoji[':1st_place_medal:'])
 
 
 
