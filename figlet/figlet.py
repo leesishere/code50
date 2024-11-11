dc9116c44b087578d83cd7b3293205dc9e7945e9
leesishere <leesishere@gmail.com>
2024-11-11 14:27:26 +0000

    Mon, Nov 11, 2024, 9:27 AM -05:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,7 +1,10 @@
 import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description='Process some arguments.')
     parser.add_argument('-f', '--font', type=str, help='Specify the font name')
+    args = parser.parse_args()
+
     if args.font:
         print(f"Font specified: {args.font}")
     else:
