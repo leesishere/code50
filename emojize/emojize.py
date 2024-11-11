b826e9628a6d51856bf8ccca1d17dd3352123554
leesishere <leesishere@gmail.com>
2024-11-11 03:38:59 +0000

    Sun, Nov 10, 2024, 10:38 PM -05:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -11,6 +11,7 @@ def main():
         input_message = input("Input: ").strip().split()
         message = ''
         for input_command_line in input_message:
+            
             if input_command_line[0] == ':' and input_command_line[-1] == ':':
                input_command_line = input_command_line.lower()
                message += emoji[input_command_line] + ' '
