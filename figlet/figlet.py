532b2db45677b6f9fcd9ab06956c13abe5349f4a
leesishere <leesishere@gmail.com>
2024-11-11 15:47:53 +0000

    Mon, Nov 11, 2024, 10:47 AM -05:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,6 +5,23 @@ from pprint import pprint
 
 
 def main():
+
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) < 3:
+        print("Usage: script.py <arg1> <arg2>")
+        sys.exit(1)
+
+    # Access individual arguments
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+
+    # Print the arguments
+    print(f"Argument 1: {arg1}")
+    print(f"Argument 2: {arg2}")
+
+if __name__ == "__main__":
+    main()
+
     parser = argparse.ArgumentParser(description='Process some arguments.')
     parser.add_argument('-f', '--font', type=str, help='Specify the font name')
     try:
