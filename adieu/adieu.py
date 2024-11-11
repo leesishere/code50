23b4b376aeba6843dc73ecd150fad64d914b78d7
leesishere <leesishere@gmail.com>
2024-11-11 20:20:48 +0000

    Mon, Nov 11, 2024, 3:20 PM -05:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -9,8 +9,10 @@ def main():
         print("Adieu, adieu, to ",end='')
         for name in name_list[0:-1]:
             print(name + ", ", end='')
-        print("and", name_list[-1])
-
+        if len(name_list) > 1:
+            print("and", name_list[-1])
+        else:
+            print(name_list[-1])
 
 
 if __name__ == "__main__":
