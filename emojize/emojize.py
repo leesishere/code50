482d2232100eab6e938e06d50b2026540cbbb2b2
leesishere <leesishere@gmail.com>
2024-11-11 13:38:25 +0000

    Mon, Nov 11, 2024, 8:38 AM -05:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -6,6 +6,17 @@ from emoji import emoji_dict
 emoji = emoji_dict
 pattern_colon = r':\w+:'
 
+def main():
+    while True:
+        input_message = input("Input: ").strip().split()
+        for input_command_line in input_message:
+            colon_words = re.findall(pattern_colon, input_command_line)
+            if colon_words:
+                punctuation = input_command_line.replace(colon_words[0],'')
+                message += f" {emoji[colon_words[0]]}{punctuation}"
+            else:
+                message += " " + input_command_line
+
 '''
 def main():
     while True:
@@ -22,10 +33,8 @@ def main():
             print(f"output: {message}")
             break
 '''
-def main():
-    while True:
-        input_message = input("Input: ").strip().split()
-        
+
+
 
 if __name__ == "__main__":
     main()
