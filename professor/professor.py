61872db04ac0249d0d7ccd47469c8924a3852219
leesishere <leesishere@gmail.com>
2024-11-11 22:58:34 +0000

    Mon, Nov 11, 2024, 5:58 PM -05:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,7 +2,7 @@ import random
 
 
 def main():
-    
+
 
 def get_level():
     while not (input_level := input("Level: ").strip()).isdigit() or not (1 <= int(input_level) <= 3): pass
@@ -11,8 +11,12 @@ def get_level():
 
 
 def generate_integer(level):
-    ...
-
-
+    if level == 1:
+        return random.randint(0, 9)
+    if level == 2:
+        return random.randint(10, 19)
+    if level == 3:
+        return random.randint(20, 29)
+    
 if __name__ == "__main__":
     main()
