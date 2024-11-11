108626a73e8f374917fd9fd0b3d73a8db0a3e120
leesishere <leesishere@gmail.com>
2024-11-11 03:45:26 +0000

    Sun, Nov 10, 2024, 10:45 PM -05:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -11,12 +11,16 @@ def main():
         input_message = input("Input: ").strip().split()
         message = ''
         for input_command_line in input_message:
+            colon_words = re.findall(pattern_colon, input_command_line)
+            print(f"{input_command_line} {colon_words}")
 
+            '''
             if input_command_line[0] == ':' and input_command_line[-1] == ':':
                input_command_line = input_command_line.lower()
                message += emoji[input_command_line] + ' '
             else:
                 message +=  input_command_line + ' '
+            '''
 
         if message:
             print(message)
