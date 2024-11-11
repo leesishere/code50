b4bdad4d1bf6a2cbed1ea2ed7670475b3d7e4e70
leesishere <leesishere@gmail.com>
2024-11-11 14:50:50 +0000

    Mon, Nov 11, 2024, 9:50 AM -05:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,6 +1,9 @@
 import argparse
 import pyfiglet
 
+from pyfiglet import Figlet
+f = Figlet(font='slant')
+print f.renderText('text to render')
 
 def main():
     parser = argparse.ArgumentParser(description='Process some arguments.')
