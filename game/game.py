286eaaf2c12b6139da0fc92ccad963ef27ac7cfc
leesishere <leesishere@gmail.com>
2024-11-11 22:23:05 +0000

    Mon, Nov 11, 2024, 5:23 PM -05:00

--- game/game.py
+++ game/game.py
@@ -2,8 +2,7 @@ import random
 
 # Prompts the user for a level,
 # If the user does not input a positive integer, the program should prompt again.
-# Randomly generates an integer between 1 and
-# inclusive, using the random module.
+# Randomly generates an integer between 1 and inclusive, using the random module.
 # Prompts the user to guess that integer. If the guess is not a positive integer, the program should prompt the user again.
 # If the guess is smaller than that integer, the program should output Too small! and prompt the user again.
 # If the guess is larger than that integer, the program should output Too large! and prompt the user again.
@@ -13,7 +12,7 @@ def main():
 
     while not (input_level := input("Level: ").strip()).isdigit(): pass
     print(input_level)
-andom_index = random.randint(0, end)    
+andom_index = random.randint(0, end)
     guess = input("Guess: ")
 
 if __name__ == "__main__":
