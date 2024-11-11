80e95596952ed0ee6e694b31a2dc62f35a15dbf8
leesishere <leesishere@gmail.com>
2024-11-11 02:06:14 +0000

    Sun, Nov 10, 2024, 9:06 PM -05:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -3,10 +3,13 @@ sys.path.append('.')
 
 from emoji import emoji_dict
 emoji = emoji_dict
-print(emoji[':1st_place_medal:'])
+
 
 def main():
-    input_emoji = input("Input").lower().strip()
+    while true:
+        input_emoji = input("Input").lower().strip()
+    try:
+        print(f"Output: {emoji[input_emoji]}")
 
 
 
