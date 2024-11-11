e47a35700e2b96b6bf19e39f93f1ef2fab149c90
leesishere <leesishere@gmail.com>
2024-11-11 03:42:20 +0000

    Sun, Nov 10, 2024, 10:42 PM -05:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -4,14 +4,14 @@ sys.path.append('.')
 
 from emoji import emoji_dict
 emoji = emoji_dict
-
+pattern_colon = r':\w+:'
 
 def main():
     while True:
         input_message = input("Input: ").strip().split()
         message = ''
         for input_command_line in input_message:
-            
+
             if input_command_line[0] == ':' and input_command_line[-1] == ':':
                input_command_line = input_command_line.lower()
                message += emoji[input_command_line] + ' '
