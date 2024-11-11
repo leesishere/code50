263c6ab4ed60a6095e3179e1f7327eeb43c6af7e
leesishere <leesishere@gmail.com>
2024-11-11 21:31:17 +0000

    Mon, Nov 11, 2024, 4:31 PM -05:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -12,10 +12,15 @@ def main():
     except EOFError:
         if name_list[-1] != '':
             print()
-            
+
         # make sure the user entered a name not a space or blank
         name_list = [name for name in name_list if name]
         print("Adieu, adieu, to ",end='')
+        if len(name_list) == 1:
+            print(name_list[-1])
+        elif: len(name_list) == 2:
+            print({name_list[0]} and {name_list[1]}")
+        elif(len(name_list) > 2)
         for name in name_list[0:-1]:
             print(name + ", ", end='')
         if len(name_list) > 1:
