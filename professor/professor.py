154f5db55a16ecc1e6b47c171b32174cb446702b
leesishere <leesishere@gmail.com>
2024-11-13 00:54:41 +0000

    Tue, Nov 12, 2024, 7:54 PM -05:00

--- professor/professor.py
+++ professor/professor.py
@@ -7,6 +7,8 @@ def main():
     while number_of_math_problems > 0:
         first_digit = generate_integer(level)
         second_digit = generate_integer(level)
+
+        print
         answer = first_digit + second_digit
         while not (input_reponse := input().strip()).isdigit(): pass
         input_reponse = int(input_reponse)
