cd1efc7557faa02c208145aea6151a993b0653b3
leesishere <leesishere@gmail.com>
2024-11-13 23:23:41 +0000

    Wed, Nov 13, 2024, 6:23 PM -05:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,6 +1,59 @@
 import requests
 import sys
 
+from pprint import pprint
+
+# Font names: http://www.figlet.org/fontdb.cgi
+
+class ParserMyCommandLine():
+    def __init__(self, flag, argv):
+
+        self.flag_list = flag.split(",")
+        self.font = self.set_font_name(argv)
+        self.font_list = self.getFonts()
+        self.argv = argv
+
+        if not self.is_comandline_correct():
+            sys.exit("Invalid usage")
+
+    def set_font_name(self, argv):
+        if len(argv) == 3:  # file_name[0] -flag[1] font_name[2]
+            return argv[2]
+        else:
+            return None
+
+    def get_font_name(self):
+        return self.font
+
+    def get_random_font(self):
+        end = len(self.font_list) - 1
+        # Generate a random number
+        random_index = random.randint(0, end)
+        return self.font_list[random_index]
+
+    def is_flag_present(self):
+        if len(self.argv) < 2:
+            return False
+        else:
+            return self.argv[1] in self.flag_list
+
+    def is_font_type_present(self):
+        return True if self.font else False
+
+    def is_font_type_in_Figlet(self):
+        return self.font in self.font_list
+
+    def is_comandline_correct(self):
+        if len(self.argv) == 2 and not self.is_flag_present():
+            return False
+        elif len(self.argv) == 3 and (
+            not self.is_flag_present() or not self.is_font_type_in_Figlet()
+        ):
+            return False
+        else:
+            return True
+
+
 try:
     print(argv[1])
 
