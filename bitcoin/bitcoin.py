0b4efacb8b76368d14efb6db818f32f4fbfa1c9a
leesishere <leesishere@gmail.com>
2024-11-14 00:30:04 +0000

    Wed, Nov 13, 2024, 7:30 PM -05:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -5,14 +5,18 @@ from pprint import pprint
 
 class CommandLineParser:
     def __init__(self):
-        self.argv = sys.argv[1:]
-
+        try:
+            self.argv = sys.argv[1:]
+        except:
+            sys.exit
+            
     def get_parm_count(self):
         return len(self.argv)
 
     def get_parm_list(self):
         return [l for l in self.argv]
-    
+
+    sys.exit
 
 try:
     p = CommandLineParser()
