f46432c3ce3124fa66f3eb9dc46f384de49a0320
leesishere <leesishere@gmail.com>
2024-11-15 01:55:56 +0000

    Thu, Nov 14, 2024, 8:55 PM -05:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -6,9 +6,15 @@ from twttr import shorten
 
 def test_shorten():
     assert shorten("Jamey") == "Jmy"
-    assert shorten("jamey") == "jmy"
-     assert shorten("JAMEY") == "JMY"
 
+def_without_vowel():
+    assert shorten("ABCDFG") == "ABCDFG"
+
+def without_capitalized():
+    assert shorten("jamey") == "jmy"
+
+def without_lowercase():
+     assert shorten("JAMEY") == "JMY"
 
 def test_error():
     with pytest.raises(TypeError):
