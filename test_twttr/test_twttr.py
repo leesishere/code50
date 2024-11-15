40945e6ecc3862f24b26182121aac8fed54c844d
leesishere <leesishere@gmail.com>
2024-11-15 01:31:59 +0000

    Thu, Nov 14, 2024, 8:31 PM -05:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -6,7 +6,7 @@ from twttr import shorten
 
 def test_shorten():
     assert shorten("Jamey") == "Jmy"
-    assert shorten("JAMEY") == "Jmy"
+
 
 def test_error():
     with pytest.raises(TypeError):
