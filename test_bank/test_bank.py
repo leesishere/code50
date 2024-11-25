f0eed5c3b64147d6e40ebeb90bb5566e72e1f755
leesishere <leesishere@gmail.com>
2024-11-25 15:14:16 +0000

    Mon, Nov 25, 2024, 9:14 AM -06:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -5,27 +5,21 @@ sys.path.append('/workspaces/21178063/bank')
 from bank import value
 
 
-def test_shorten():
-    assert value("Jamey") == 0
+def test_not_hello():
+    assert value("Jamey") == 100
 
-def main():
-    greeting = input("Greating: ").lower().strip()
-    pay_up = value(greeting)
+def test_hello_upper():
+    assert value("HELLO") == 0
 
-    if pay_up == 0:
-        print("$0")
-    elif pay_up == 20:
-        print("$20")
-    elif pay_up == 100:
-        print("$100")
+def test_hello_lower():
+    assert value("hello") == 0
 
-def value(greeting):
-    if 'hello' == greeting[:5]:
-        return 0
-    elif 'h' == greeting[0]:
-        return 20
-    else:
-        return 100
+def test_hello_mixed():
+    assert value("heLLo") == 0
 
-if __name__ == "__main__":
-    main()
+def test_just_start_with_h():
+    assert value("howdy") == 20
+
+def test_just_start_with_h_two():
+    assert value("hi") == 20
+
