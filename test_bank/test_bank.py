be0fb46753f76535d1b0fc71fe767fb9e7ad2381
leesishere <leesishere@gmail.com>
2024-11-25 14:59:02 +0000

    Mon, Nov 25, 2024, 8:59 AM -06:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,5 +1,5 @@
 def main():
-    greating = input("Greating: ").lower().strip()
+    greeting = input("Greating: ").lower().strip()
     pay_up = value(greeting)
 
     if pay_up == 0:
@@ -10,9 +10,9 @@ def main():
         print("$100")
 
 def value(greeting):
-    if 'hello' == greating[:5]:
+    if 'hello' == greeting[:5]:
         return 0
-    elif 'h' == greating[0]:
+    elif 'h' == greeting[0]:
         return 20
     else:
         return 100
