888784fc7be8e71f3a768c319a4b0cfd5acfa900
leesishere <leesishere@gmail.com>
2024-11-25 15:46:37 +0000

    Mon, Nov 25, 2024, 9:46 AM -06:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -4,7 +4,14 @@ sys.path.append('/workspaces/21178063/plates')
 
 from plates import is_valid
 
+# “All vanity plates must start with at least two letters.”
+# “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”#
 
-def test_not_hello():
-    assert value("Jamey") == 100
+# Numbers cannot be used in the middle of a plate; they must come at the end.
+# For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
+# CS50P2
+
+
+def is_valid():
+    assert value("Ja") == True
 
