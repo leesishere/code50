f3a6c3a7e280283495862b3282a9545e24a1a146
leesishere <leesishere@gmail.com>
2024-11-25 14:41:41 +0000

    Mon, Nov 25, 2024, 8:41 AM -06:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,1 +1,15 @@
+def main():
+    greating = input("Greating: ").lower().strip()
+    if 'hello' == greating[:5]:
+        print("$0")
+    elif 'h' == greating[0]:
+        print("$20")
+    else:
+        print("$100")
 
+def value(greeting):
+    ...
+
+
+if __name__ == "__main__":
+    main()
