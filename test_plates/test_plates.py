1bd5d1b88c00f1202d43e564f9f9e9fa342d13d9
leesishere <leesishere@gmail.com>
2024-11-25 15:40:39 +0000

    Mon, Nov 25, 2024, 9:40 AM -06:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,5 +1,10 @@
 import sys
 import pytest
-sys.path.append('/workspaces/21178063/bank')
+sys.path.append('/workspaces/21178063/plates')
 
-from bank import value
+from plates import is_valid
+
+
+def test_not_hello():
+    assert value("Jamey") == 100
+
