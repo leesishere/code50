c8a33d335e94029817368183634ea31c71022278
leesishere <leesishere@gmail.com>
2024-11-26 17:27:51 +0000

    Tue, Nov 26, 2024, 11:27 AM -06:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -27,7 +27,7 @@ def test_is_valid_number_middle():
 def test_is_valid_number_last():
     assert is_valid("AAA222") == True
 
-def test_is_invalid_number_first():
+def test_without_beginning_alphabetical_checks():
     assert is_valid("22AAAA") == False
 
 def test_is_invalid_start_with_zero():
