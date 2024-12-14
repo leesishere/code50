0a1f18af7d61380f14b84e6382cf950ade0efcc1
leesishere <leesishere@gmail.com>
2024-12-14 16:23:44 +0000

    Sat, Dec 14, 2024, 10:23 AM -06:00

--- plates/plates.py
+++ plates/plates.py
@@ -64,7 +64,8 @@ def count_numbers(s):
 
 def order_of_things(s):
     # Define the pattern to split by digits and non-digits
-    pattern = r'[a-zA-Z]+|\d+'
+    #pattern = r'[a-zA-Z]+|\d+'
+    pattern = f'[a-zA-Z0-9]'
     return re.findall(pattern, s)
 
 if __name__ == "__main__":
