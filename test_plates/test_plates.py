9224d03139c55d6d5c5afa201618f54f8a16eb4c
leesishere <leesishere@gmail.com>
2024-12-14 18:41:29 +0000

    Sat, Dec 14, 2024, 12:41 PM -06:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -40,7 +40,7 @@ def test_is_invalid_alphanumeric_characters():
     assert is_valid("AAAA?A") == False
 
 def test_is_beginning_alphabetical_checks():
-    assert is_valid("!AAAAA") == False
+    assert is_valid("1AAAAA") == False
 
 def test_is_invalid_cs_class():
     assert is_valid("CS05") == False
