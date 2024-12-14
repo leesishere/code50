b312e9fa4fba675c137d7ced9ba1dfb3fe981a63
leesishere <leesishere@gmail.com>
2024-12-14 20:34:08 +0000

    Sat, Dec 14, 2024, 2:34 PM -06:00

--- plates/plates.py
+++ plates/plates.py
@@ -66,15 +66,10 @@ def is_valid(s):
     # not begining alphabetical
     plate_string = order_of_things(s)
     letters_count = count_letters(s)
-    print(letters_count)
     alpha = ['A','B','C','D','E','F']
-    if plate_string[:letters_count] == alpha[:len(plate_string)]:
-        print("The start of alpha is the same as plate entered.")
+    if plate_string[:letters_count] == alpha[:letters_count]:
+        return False
 
-    #print(f"{plate_string}")
-
-    #if s[:2].upper() == 'AB':
-    #    return False
 
     return True
 
