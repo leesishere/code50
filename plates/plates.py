8b7419a1dc783f5b42c5161c4a1c66944eafec5f
leesishere <leesishere@gmail.com>
2024-12-14 21:16:23 +0000

    Sat, Dec 14, 2024, 3:16 PM -06:00

--- plates/plates.py
+++ plates/plates.py
@@ -6,10 +6,12 @@ def main():
     plate = input("Plate: ")
     if is_valid(plate):
         print("Valid")
-        sys.exit(0)
+        return 0
+        #sys.exit(0)
     else:
         print("Invalid")
-        sys.exit(1)
+        return 1
+        #sys.exit(1)
 
 
 def is_valid(s):
