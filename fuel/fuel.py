7b5d2b3d94764ebe0139ed1ee445d442cb5f0a69
leesishere <leesishere@gmail.com>
2024-12-18 21:11:59 +0000

    Wed, Dec 18, 2024, 3:11 PM -06:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -23,6 +23,7 @@ def gauge(percentage):
     return print_string
 
 def gauge_(percentage):
+    print(percentage)
     if percentage < 2:
         print_string = f"E"
     elif percentage > 89:
--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -13,14 +13,14 @@ from fuel import gauge
 def test_convert():
     assert convert("1/4") == 25
     assert convert("3/4") == 75
-    assert convert("4/4") == 1
+    assert convert("4/4") == 100
     assert convert("0/4") == 0
 
 
 def test_gauge():
     assert gauge(25) == '25%'
     assert gauge(75) == '75%'
-    assert gauge(1) == 'F'
+    assert gauge(99) == 'F'
     assert gauge(0) == 'E'
 
 def test_ValueError():
