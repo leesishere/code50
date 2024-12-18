e780f01d7d601d56aec17a24348bbcadb5ec0e7c
leesishere <leesishere@gmail.com>
2024-12-18 21:13:41 +0000

    Wed, Dec 18, 2024, 3:13 PM -06:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -21,7 +21,7 @@ def test_gauge():
     assert gauge(25) == '25%'
     assert gauge(75) == '75%'
     assert gauge(99) == 'F'
-    assert gauge(0) == 'E'
+    assert gauge(1) == 'E'
 
 def test_ValueError():
     with pytest.raises(ValueError):
