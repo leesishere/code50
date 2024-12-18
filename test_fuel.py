e934c633a0bfb741a518058c51def08b5e4e1204
leesishere <leesishere@gmail.com>
2024-12-18 18:02:34 +0000

    Wed, Dec 18, 2024, 12:02 PM -06:00

--- test_fuel.py
+++ test_fuel.py
@@ -2,4 +2,14 @@ import sys
 import pytest
 sys.path.append('/workspaces/21178063/fuel')
 
+from fuel import convert
+from fuel import gauge
+
+# convert expects a str in X/Y format as input, wherein each of X and Y is an integer,
+# and returns that fraction as a percentage rounded to the nearest int between 0 and 100, inclusive.
+# If X and/or Y is not an integer, or if X is greater than Y, then convert should raise a ValueError.
+# If Y is 0, then convert should raise a ZeroDivisionError.
+
+def test_shorten():
+    assert convert("1/4") == "25%"
 
