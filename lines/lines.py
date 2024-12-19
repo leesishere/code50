dad814480a313a85c2721c96d9cb910653e95055
leesishere <leesishere@gmail.com>
2024-12-19 16:33:19 +0000

    Thu, Dec 19, 2024, 10:33 AM -06:00

--- lines/lines.py
+++ lines/lines.py
@@ -26,24 +26,10 @@ def count_code_lines(file_path):
             results = remove_blank_lines(results)
             print(f"remove_blank_lines = {len(results.split("\n"))}")
 
-            print("*" * 10)
-            print(results)
-            print("*" * 10)
-            # Find the lines with triple quotes
-            result = remove_DocString(results)
-            print("Lines with triple quotes:")
-            for match in result:
-                print(f"'''{match}'''")
-
 
     except FileNotFoundError:
         print(f"The file {file_path} does not exist.")
 
-def remove_DocString(text):
-    # Regular expression to find lines that start and end with triple quotes
-    pattern = r"^'''(.*?)'''$"
-    matches = re.findall(pattern, text, re.MULTILINE)
-    return matches
 
 
 def remove_blank_lines(text):
