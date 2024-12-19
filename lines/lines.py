1aeed14de22b48e96aa2e8f47dc168e37eb7376e
leesishere <leesishere@gmail.com>
2024-12-19 15:06:32 +0000

    Thu, Dec 19, 2024, 9:06 AM -06:00

--- lines/lines.py
+++ lines/lines.py
@@ -9,10 +9,10 @@ def main():
 def check_arguments(params):
     if len(params) == 1:
         print("Too few command-line arguments")
-        sys.exit()
+        sys.exit(1)
     if len(params) > 2:
         print("Too many command-line arguments")
-        sys.exit()
+        sys.exit(1)
     else:
         return params[1]
 
@@ -20,13 +20,13 @@ def file_exists(file_path):
     # Check if the file extension is .py
     if not py_extension(file_path):
         print("Not a Python file")
-        sys.exit()
-        return False
+        sys.exit(1)
     # Check if the file exists
     if os.path.isfile(file_path):
         return True
     else:
-        return False
+        print("File does not exist")
+        sys.exit(1)
 
 def py_extension(file_path):
     file_extension = os.path.splitext(file_path)[1]
