305a9bc0e8b7d0c89d37a23f23a4bc07fee76373
leesishere <leesishere@gmail.com>
2024-12-19 14:42:10 +0000

    Thu, Dec 19, 2024, 8:42 AM -06:00

--- lines/lines.py
+++ lines/lines.py
@@ -7,12 +7,22 @@ def main(file_path):
 
 
 def file_exists(file_path):
+    # Check if the file extension is .py
+    if not py_extension(file_path):
+        return False
     # Check if the file exists
     if os.path.isfile(file_path):
         return True
     else:
         return False
 
+def py_extension(file_path):
+    file_extension = os.path.splitext(file_path)[1]
+    
+    if file_extension.upper() == 'PY':
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main(sys.argv[1])
