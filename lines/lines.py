049f6389497da57a3cc99bdfc50a38a938a143f3
leesishere <leesishere@gmail.com>
2024-12-19 14:33:47 +0000

    Thu, Dec 19, 2024, 8:33 AM -06:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,4 +1,18 @@
 import sys
+import os
 
-print(sys.argv[1:])
+def main(file_path):
+    print(file_path)
 
+
+    # Check if the file exists
+   
+    if os.path.isfile(file_path):
+        print("File exists")
+    else:
+        print("File does not exist")
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
