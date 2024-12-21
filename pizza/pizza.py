0c8f27246ffd1b9a5b0d1035a9da7fc4f1889803
leesishere <leesishere@gmail.com>
2024-12-21 15:25:11 +0000

    Sat, Dec 21, 2024, 9:25 AM -06:00

---
+++ pizza/pizza.py
@@ -0,0 +1,37 @@
+def main():
+    
+
+
+
+def check_arguments(params):
+    if len(params) == 1:
+        print("Too few command-line arguments")
+        sys.exit(1)
+    if len(params) > 2:
+        print("Too many command-line arguments")
+        sys.exit(1)
+    else:
+        return params[1]
+
+def py_file_exists(file_path):
+    # Check if the file extension is .py
+    if not py_extension(file_path):
+        print("Not a Python file")
+        sys.exit(1)
+
+    # Check if the file exists
+    if os.path.isfile(file_path):
+        return True
+    else:
+        print("File does not exist")
+        sys.exit(1)
+
+def py_extension(file_path):
+    file_extension = os.path.splitext(file_path)[1]
+    if file_extension == '.py':
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+      main()
