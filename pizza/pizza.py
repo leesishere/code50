6ff5330124c83fbb19b716ac292c2be741a3874b
leesishere <leesishere@gmail.com>
2024-12-21 15:36:51 +0000

    Sat, Dec 21, 2024, 9:36 AM -06:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -8,11 +8,12 @@ def main():
 def csv_file(argv):
    if not check_arguments(argv):
         sys.exit(1)
-    argv[1]
-    if not file_exists(file_path):
-        sys.exit(1)
-    if not csv_extension(file_path):
-        sys.exit(1)
+    else:
+        #check passed file exists and is csv file 
+        if not file_exists(arg[1]):
+            sys.exit(1)
+        if not csv_extension(arg[1]):
+            sys.exit(1)
 
 def check_arguments(params):
     if len(params) == 2:
