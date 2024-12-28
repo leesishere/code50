eda4ac65f03e3f31591481b4c6b04df8e363236c
leesishere <leesishere@gmail.com>
2024-12-28 01:37:51 +0000

    Fri, Dec 27, 2024, 7:37 PM -06:00

--- shirt/sample.py
+++ shirt/sample.py
@@ -1,7 +1,20 @@
+import sys
 from PIL import Image
 
-# Open an image file
-image = Image.open('before1.jpg')
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python display_image.py <path_to_image>")
+        return
 
-# Display the image
-image.show()
+    image_path = sys.argv[1]
+
+    try:
+        # Open an image file
+        image = Image.open(image_path)
+        # Display the image
+        image.show()
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
