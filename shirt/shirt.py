213dd0f9c49c22c1c89d721f02f4e519bcfd42e1
leesishere <leesishere@gmail.com>
2024-12-28 01:54:05 +0000

    Fri, Dec 27, 2024, 7:54 PM -06:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -26,10 +26,11 @@ def main():
     out_image = sys.argv[2]
     width, height = in_image.size
     print(f"Image size: {width} x {height} pixels")
-    resized_image = image.resize((600, 600))
-# Save the resized image
-    resized_image.save('resized_' + image_path)
-    resized_image.save(new_file_name)
+    resized_image = in_image.resize((600, 600))
+
+    width, height = out_image.size
+    resized_image.save(out_image)
+    print(f"Image size: {width} x {height} pixels")
 
 def check_arguments(params):
     if len(params) == 3:
