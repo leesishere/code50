0e411ce3f91d5675a3e62d3dbcd93d9623af877a
leesishere <leesishere@gmail.com>
2024-12-29 19:09:05 +0000

    Sun, Dec 29, 2024, 1:09 PM -06:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import sys
 import os
 
@@ -13,22 +13,50 @@ def main():
             print("Too many command-line arguments")
             sys.exit(1)
     else:
-        if not extension(sys.argv[1]):
+        if not extension(sys.argv[1], sys.argv[2]):
             print("Input and output have different extensions")
             sys.exit(1)
-        if not extension(sys.argv[2]):
+        if not validate_extension(sys.argv[2]):
             print("Invalid output")
             sys.exit(1)
 
     # Open an image file
 
-    in_image = Image.open(sys.argv[1])
-    out_image = sys.argv[2]
-    width, height = in_image.size
-    print(f"Image size: {width} x {height} pixels")
-    resized_image = in_image.resize((600, 600))
-    resized_image.save(out_image)
+    background_image_path = sys.argv[1]
+    output_path = sys.argv[2]
 
+    overlay_path = 'shirt.png'
+    # Open the overlay and background images
+    overlay = Image.open(overlay_path)
+    background = Image.open(background_image_path)
+
+
+
+    # Get the dimensions of the background image
+    width, height = background.size
+    # Define the crop box (left, upper, right, lower)
+    top_pixels = 0
+    if "1" in background_image_path:
+        crop_box = (0, top_pixels + 75, width + 0, height - 75)
+    else:
+        crop_box = (0, top_pixels + 200, width + 0, height - 200)
+    # Crop the image
+    background = background.crop(crop_box)
+
+    # Resize the background image to fit the overlay size
+    #overlay = overlay.resize(background.size, Image.LANCZOS)
+    background = background.resize(overlay.size, Image.LANCZOS)
+
+
+    # Set the position for the overlay (e.g., center it on the background)
+    position = (0, -40)
+    position = (0, 0)
+
+    # Top-left corner
+    # # Overlay the image with transparency handling
+    background.paste(overlay, position, overlay)
+    # Save the result
+    background.save(output_path)
 
 def check_arguments(params):
     if len(params) == 3:
@@ -42,10 +70,18 @@ def file_exists(file_path):
         return True
     else:
         return False
+def validate_extension(file_extension):
+    extension = os.path.splitext(file_extension)[1]
+    if (extension == '.jpg' or extension == '.png'):
+        return True
+    else:
+        return False
 
-def extension(file_path):
-    file_extension = os.path.splitext(file_path)[1]
-    if file_extension == '.jpg' or file_extension == '.png':
+def extension(file_path_one, file_path_two):
+    file_extension_one = os.path.splitext(file_path_one)[1]
+    file_extension_two = file_path_two.split(".")[1]
+    file_extension_two = "." + file_extension_two
+    if file_extension_one == file_extension_two:
         return True
     else:
         return False
