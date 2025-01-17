b9ab28c92cf598726dc91d3076f6440a20170a8a
leesishere <leesishere@gmail.com>
2025-01-17 15:09:06 +0000

    Fri, Jan 17, 2025, 10:09 AM -05:00

--- shirt/sample.py
+++ shirt/sample.py
@@ -1,24 +1,33 @@
-import cv2
 from PIL import Image, ImageOps
+import numpy as np
 
-# Load images using open cv
-img = cv2.imread("before1.jpg")
-shirt_img =  cv2.imread("shirt.png")
+# Load images from file
+img = Image.open("before1.jpg")
+shirt_img = Image.open("shirt.png")
 
 # Resize and crop the input image
 resized_img = img.copy()
-cv2.resize(resized_img,(1000,600))
-width,height= resized_img.shape[1],resized_img.shape[0]
-cropped= resized_img[int(height/3):int(2*height/3), int(width/3):int(2*width/3)]
+width, height = resized_img.size
+cropped_width, cropped_height = 1000, 600
+cropped_rect = (height // 3, width // 3, cropped_height + height // 6 * 2, cropped_width + width // 6 * 2)
+resized_cropped = img.crop(cropped_rect)
 
 # Overlay the shirt image
-overlayed = Image.fromarray(cropped)
-shirt_pil =  Image.open("shirt.png")
-shirtscaled=shirt_pil.resize((cropped.shape[1], cropped.shape[0]))
-overlaid_shirt=ImageOps.fit(shirtscaled, overlayed.size, method=2, bleed=(20.0/100*overlayed.size), centering=(50.0/100*overlayed.size))
-composing_img= Image.new('RGB', (cropped.shape[1], cropped.shape[0]), color = 255)
-overlaid_shirt.paste(overlayed, mask=None)
-composing_img.paste(composing_img, mask=None)
+shirt_scaled = shirt_img.resize(resized_cropped.size)
+overlayed_shirt = ImageOps.fit(shirt_scaled, resized_cropped.size, method=2, bleed=(0.20), centering=(0.50))
+overlaid_shirt = overlayed_shirt.convert('RGBA')
+
+# Create a new image to hold the composition
+composing_img = Image.new('RGB', (cropped_width, cropped_height), color=(255, 255, 255))
+
+# Paste the overlaid shirt onto the composinimg
+for x in range(cropped_width):
+    for y in range(cropped_height):
+        pixel_value = overlayed_shirt.getpixel((x, y))
+        composing_img.putpixel((x + cropped_rect[0], y + cropped_rect[1]), (int(pixel_value[0]),
+                                                                int(pixel_value[1]),
+                                                                int(pixel_value[2]),
+                                                                255))
 
 # Save the resulting image
-cv2.imwrite("after1.png", cv2.cvtColor(np.array(composing_img), cv2.COLOR_RGB2BGR))
+overlaid_shirt.save("after1.png")
