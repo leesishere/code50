eeace70ec0f4e3c978fc9ad5b9fa21f9741fce96
leesishere <leesishere@gmail.com>
2025-01-17 15:06:57 +0000

    Fri, Jan 17, 2025, 10:06 AM -05:00

--- shirt/sample.py
+++ shirt/sample.py
@@ -1,20 +1,24 @@
-import sys
-from PIL import Image
+import cv2
+from PIL import Image, ImageOps
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: python display_image.py <path_to_image>")
-        return
+# Load images using open cv
+img = cv2.imread("before1.jpg")
+shirt_img =  cv2.imread("shirt.png")
 
-    image_path = sys.argv[1]
+# Resize and crop the input image
+resized_img = img.copy()
+cv2.resize(resized_img,(1000,600))
+width,height= resized_img.shape[1],resized_img.shape[0]
+cropped= resized_img[int(height/3):int(2*height/3), int(width/3):int(2*width/3)]
 
-    try:
-        # Open an image file
-        image = Image.open(image_path)
-        # Display the image
-        image.show()
-    except Exception as e:
-        print(f"Error: {e}")
+# Overlay the shirt image
+overlayed = Image.fromarray(cropped)
+shirt_pil =  Image.open("shirt.png")
+shirtscaled=shirt_pil.resize((cropped.shape[1], cropped.shape[0]))
+overlaid_shirt=ImageOps.fit(shirtscaled, overlayed.size, method=2, bleed=(20.0/100*overlayed.size), centering=(50.0/100*overlayed.size))
+composing_img= Image.new('RGB', (cropped.shape[1], cropped.shape[0]), color = 255)
+overlaid_shirt.paste(overlayed, mask=None)
+composing_img.paste(composing_img, mask=None)
 
-if __name__ == "__main__":
-    main()
+# Save the resulting image
+cv2.imwrite("after1.png", cv2.cvtColor(np.array(composing_img), cv2.COLOR_RGB2BGR))
