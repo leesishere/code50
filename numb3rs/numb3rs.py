797f3fdcbdd2126338b4ef1d1bb0a1e7f8cef5a7
leesishere <leesishere@gmail.com>
2025-01-29 23:40:08 +0000

    Wed, Jan 29, 2025, 6:40 PM -05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,5 +1,6 @@
 import re
 import sys
+from ipaddress import ip_address
 
 # IP address
 # ###.###.###.### But each # should be a number between 0 and 255,
@@ -9,10 +10,15 @@ def main():
 
 
 def validate(ip):
-    ...
 
 
-...
+
+def validIP(IP: str) -> str:
+    try:
+        ip = ip_address(IP)
+        return "IPv4" if ip.version == 4 else "IPv6"
+    except ValueError:
+        return "Invalid"
 
 
 if __name__ == "__main__":
