259625bda8554883b18e05438aca0b4bf185ffb4
leesishere <leesishere@gmail.com>
2025-01-29 23:52:34 +0000

    Wed, Jan 29, 2025, 6:52 PM -05:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,20 +5,11 @@ sys.path.append('/workspaces/21178063/numb3rs')
 from validate import numb3rs
 
 
-def test_():
-    assert validate("Jamey") == 100
+def test_True():
+    assert validate("255.255.255.255") == True
+    assert validate("1.2.3.4") == True
 
-def test_hello_upper():
-    assert value("HELLO") == 0
-
-def test_hello_lower():
-    assert value("hello") == 0
-
-def test_hello_mixed():
-    assert value("heLLo") == 0
-
-def test_just_start_with_h():
-    assert value("howdy") == 20
-
-def test_just_start_with_h_two():
-    assert value("hi") == 20
+def test_False():
+    assert validate("512.512.512.512") == False
+    assert validate("1.2.3.1000") == False
+    assert validate("cat") == False
