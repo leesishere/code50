388dff7a25cea0604577298df7a485b06592cac2
leesishere <leesishere@gmail.com>
2025-01-30 10:45:37 +0000

    Thu, Jan 30, 2025, 5:45 AM -05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -13,7 +13,7 @@ def validate(ip):
     pattern_IPv4 = r"\b([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\b"
     match = re.search(pattern_IPv4, ip)
 
-    pattern_nonIPv4 = r"\b([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\b(?!\.)"
+    pattern_nonIPv4 = r"\b^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\b(?!\.)"
     match_non = re.search(pattern_nonIPv4, ip)
 
     # verify there are only 4 octial
@@ -27,7 +27,7 @@ def validate(ip):
         # Loop through the groups and print each one
         for i in range(1, num_groups + 1):
             print(f"Group {i}: {match_non.group(i)}")
-
+'''
     if match:
         # Get the number of groups
         num_groups = match.lastindex
@@ -37,7 +37,7 @@ def validate(ip):
         for i in range(1, num_groups + 1):
             print(f"Group {i}: {match.group(i)}")
 
-
+'''
 '''
 def validate(IP: str) -> str:
     try:
