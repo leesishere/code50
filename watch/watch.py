8bfe7dde9293a92b4f7c735e2a6bbf2ec83d67ac
leesishere <leesishere@gmail.com>
2025-01-30 11:14:39 +0000

    Thu, Jan 30, 2025, 6:14 AM -05:00

---
+++ watch/watch.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+# sample input:
+# <iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
+#
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
