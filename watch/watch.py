454811bf2d2fac75fb5a514865da6e3f547c3113
leesishere <leesishere@gmail.com>
2025-01-30 11:17:14 +0000

    Thu, Jan 30, 2025, 6:17 AM -05:00

--- watch/watch.py
+++ watch/watch.py
@@ -3,6 +3,8 @@ import sys
 
 # sample input:
 # <iframe src="http://www.youtube.com/embed/xvFZjo5PgG0"></iframe>
+# <iframe width="560" height="315" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 # return value:
 # https://youtu.be/xvFZjo5PgG0
 
