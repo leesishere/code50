544363fb2b2e79a5d2a2b0ee337d65bfdf2fb214
leesishere <leesishere@gmail.com>
2025-01-30 11:57:22 +0000

    Thu, Jan 30, 2025, 6:57 AM -05:00

--- watch/watch.py
+++ watch/watch.py
@@ -18,7 +18,7 @@ def parse(s):
 
     if match:
         url = match.group(1)
-        pattern = r'\/(.+)$'
+        pattern = r'https?://(?:www\.)youtube.com/embed/'
         match = re.search(pattern, url)
         print(match.group(1))
 
