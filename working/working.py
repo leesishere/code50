8ff520f8f416e72e9601c12015b28848f644dcfa
leesishere <leesishere@gmail.com>
2025-02-01 04:29:13 +0000

    Fri, Jan 31, 2025, 11:29 PM -05:00

--- working/working.py
+++ working/working.py
@@ -14,7 +14,7 @@ def main():
 def convert(s):
     s = s.strip()
     pattern = r'^(1[0-2]|0?[1-9]):?([0-5][0-9])? ?([AaPp][Mm])\s+[Tt][Oo]\s+(1[0-2]|0?[1-9]):?([0-5][0-9])? ?([AaPp][Mm])$'
-    pattern = r'(1[0-2]|0?[1-9]):?([0-5][0-9])? ?([AaPp][Mm]\s[Tt][Oo]\s(1[0-2]|0?[1-9]):?([0-5][0-9])? ?([AaPp][Mm]))'
+    pattern = r'(1[0-2]|0?[1-9]):?([0-5][0-9])? ?([AaPp][Mm])\s[Tt][Oo]\s(1[0-2]|0?[1-9]):?([0-5][0-9])? ?([AaPp][Mm])'
 
     #pattern = r'http(?:s):\/\/(?:www\.)?youtube\.com\/(?:embed/)?'
     match = re.search(pattern, s)
@@ -26,7 +26,7 @@ def convert(s):
         #secondHour = match.group(4)
         #secondMinute = match.group(5)
         #secondMeridieme = match.group(6)
-        print(f"{firstHour} {firstMinute} {firstMeridieme}")
+        print(f"{firstHour} \n {firstMinute} \n{firstMeridieme}")
         #print(f"{secondHour} {secondMinute} {secondMeridieme}")
     except:
         pass
