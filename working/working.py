d4dd819f78c9813fbe8d72a6a4fd4dd43573e308
leesishere <leesishere@gmail.com>
2025-02-01 03:00:23 +0000

    Fri, Jan 31, 2025, 10:00 PM -05:00

--- working/working.py
+++ working/working.py
@@ -6,13 +6,13 @@ def main():
     print(convert(input("Hours: ")))
 
 
-9:00 AM to 5:00 PM
-9 AM to 5 PM
-9:00 AM to 5 PM
-9 AM to 5:00 PM
+#  9:00 AM to 5:00 PM
+#  9 AM to 5 PM
+#  9:00 AM to 5 PM
+#  9 AM to 5:00 PM
 
 def convert(s):
-    
+
 
 if __name__ == "__main__":
     main()
