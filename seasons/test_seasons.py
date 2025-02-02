e21d8da08219fdf38609dd43029d9cbab3a9c0c9
leesishere <leesishere@gmail.com>
2025-02-02 20:44:46 +0000

    Sun, Feb 2, 2025, 3:44 PM -05:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,2 +1,16 @@
 import sys
 sys.path.append('/workspaces/21178063/seasons/')
+
+
+Jamuary 1, 1999
+Invalid Date
+
+Date of borth (YYYY-MM-DD):  1999-01-01
+five hundred and twenty-five thousand, six hundred
+
+Date of borth (YYYY-MM-DD):  1999-12-31
+one thousand, four hundred and forty
+
+1970-01-01
+fifteen million, seven hundred and seventy-eight thousand and eighty
+
