d44d9f37fbf3e82012137cab2b045b158f14debf
leesishere <leesishere@gmail.com>
2025-02-02 20:51:42 +0000

    Sun, Feb 2, 2025, 3:51 PM -05:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -17,5 +17,8 @@ fifteen million, seven hundred and seventy-eight thousand and eighty
 
 
 Ensure you have a correct version of seasons.py. Run your tests by executing pytest test_seasons.py. pytest should show that all of your tests have passed.
-Modify one of the functions you’ve implemented in seasons.py and imported into test_seasons.py. One of your functions might, for example, fail to raise a ValueError when it should. Run your tests by executing pytest test_seasons.py. pytest should show that at least one of your tests has failed.
-Continue to modify the behavior of seasons.py, creating (predictably) incorrect versions of your implementation. Run your tests by executing pytest test_seasons.py. Do the tests you expect to fail, fail?
+Modify one of the functions you’ve implemented in seasons.py and imported into test_seasons.py. One of your functions might,
+for example, fail to raise a ValueError when it should. Run your tests by executing pytest test_seasons.py. pytest should
+show that at least one of your tests has failed.
+Continue to modify the behavior of seasons.py, creating (predictably) incorrect versions of your implementation.
+Run your tests by executing pytest test_seasons.py. Do the tests you expect to fail, fail?
