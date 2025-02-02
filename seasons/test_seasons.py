5ed05f8dca21864a0633f2303aaa6ad48dda25fd
leesishere <leesishere@gmail.com>
2025-02-02 21:45:19 +0000

    Sun, Feb 2, 2025, 4:45 PM -05:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -10,11 +10,9 @@ def test_incorrect():
 
 
 @freeze_time("2000-01-01")
-
-
 def test_correct_2000_01_01():
     assert age_in_minutes('1999-01-01') == 'five hundred and twenty-five thousand, six hundred'
-    
+
     #assert age_in_minutes('1999-12-31') == 'one thousand, four hundred and forty'
     #assert age_in_minutes('1970-01-01') == 'fifteen million, seven hundred and seventy-eight thousand and eighty'
 
