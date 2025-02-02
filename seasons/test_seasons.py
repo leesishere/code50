484e01c8c4a58a773358fedf31e9c2b35c2021fc
leesishere <leesishere@gmail.com>
2025-02-02 20:56:38 +0000

    Sun, Feb 2, 2025, 3:56 PM -05:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -1,24 +1,15 @@
 import sys
 sys.path.append('/workspaces/21178063/seasons/')
+from seasons import age_in_minutes
 
+def test_correct():
+    assert age_in_minutes('Jamuary 1, 1999') == 'Invalid Date'
 
-Jamuary 1, 1999
-Invalid Date
-
-Date of borth (YYYY-MM-DD):  1999-01-01
-five hundred and twenty-five thousand, six hundred
-
-Date of borth (YYYY-MM-DD):  1999-12-31
-one thousand, four hundred and forty
-
-1970-01-01
-fifteen million, seven hundred and seventy-eight thousand and eighty
+def test_incorrect():
+    assert age_in_minutes('1999-01-01') == 'five hundred and twenty-five thousand, six hundred'
+    assert age_in_minutes('1999-12-31') == 'one thousand, four hundred and forty'
+    assert age_in_minutes('1970-01-01') == 'fifteen million, seven hundred and seventy-eight thousand and eighty'
 
 
 
-Ensure you have a correct version of seasons.py. Run your tests by executing pytest test_seasons.py. pytest should show that all of your tests have passed.
-Modify one of the functions you’ve implemented in seasons.py and imported into test_seasons.py. One of your functions might,
-for example, fail to raise a ValueError when it should. Run your tests by executing pytest test_seasons.py. pytest should
-show that at least one of your tests has failed.
-Continue to modify the behavior of seasons.py, creating (predictably) incorrect versions of your implementation.
-Run your tests by executing pytest test_seasons.py. Do the tests you expect to fail, fail?
+#for example, fail to raise a ValueError when it should. Run your tests by executing pytest test_seasons.py. pytest should
