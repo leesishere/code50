0e456a9398d122ecd3ed53cdc458bcef980691b0
leesishere <leesishere@gmail.com>
2025-02-02 20:00:50 +0000

    Sun, Feb 2, 2025, 3:00 PM -05:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -11,6 +11,13 @@ def age_in_minutes(s):
  return date.fromisoformat(s)
  #return date.today()
 
+# Calculate the difference
+difference = date2 - date1
+
+# Get the total minutes
+minutes = difference.total_seconds() / 60
+
+print(f"Minutes between dates: {minutes}")
 
 if __name__ == "__main__":
     main()
