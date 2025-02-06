cad96a6bddabf9ae2fec1f4e1ef6e016dfc51c9c
leesishere <leesishere@gmail.com>
2025-02-06 02:07:04 +0000

    Wed, Feb 5, 2025, 9:07 PM -05:00

--- jar/shirtificate/shirtificate.py
+++ jar/shirtificate/shirtificate.py
@@ -4,7 +4,8 @@ pdf = fpdf.FPDF(orientation="landscape", format="A4")
 pdf.add_page()
 pdf.image("shirtificate.png", x=20, y=60)
 pdf.set_font("Helvetica", size=8)
-pdf.set_fill_color(255, 255, 0)
+pdf.set_fill_color(255, 255, 255) # White
+
 pdf.multi_cell(w=50, text=LOREM_IPSUM[:100], new_x="LEFT", fill=True)
 pdf.ln()
 pdf.set_stretching(150)
