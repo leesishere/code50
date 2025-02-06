8d06a6f44b88be531a402c1c4f98192d2146688f
leesishere <leesishere@gmail.com>
2025-02-06 01:40:40 +0000

    Wed, Feb 5, 2025, 8:40 PM -05:00

--- jar/shirtificate/shirtificate.py
+++ jar/shirtificate/shirtificate.py
@@ -1,15 +1,9 @@
 from fpdf import FPDF
 
-#pdf = fpdf.FPDF(orientation="landscape", format="A4",(210, 297))
-#pdf = FPDF('P', 'mm', (100, 150))
-fpdf = FPDF(orientation = 'landscape', unit='(210, 297)', format='A4')
-
+pdf = fpdf.FPDF(orientation="landscape", format="A4")
 # 210mm wide by 297mm tall.
 
 
-
-pdf = FPDF()
-pdf.add_page()
 pdf.set_font("helvetica", style="B", size=16)
 pdf.cell(40, 10, "Hello World!")
 pdf.output("tuto1.pdf")
