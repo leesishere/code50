4f1a57cb896314daab511679a3748c4706770a81
leesishere <leesishere@gmail.com>
2025-02-06 01:54:35 +0000

    Wed, Feb 5, 2025, 8:54 PM -05:00

--- jar/shirtificate/shirtificate.py
+++ jar/shirtificate/shirtificate.py
@@ -8,4 +8,9 @@ pdf.set_font("helvetica", style="B", size=16)
 pdf.cell(40, 10, "Hello World!")
 pdf.output("tuto1.pdf")
 
+from fpdf import FPDF
 
+pdf = FPDF()
+pdf.add_page()
+pdf.image("docs/fpdf2-logo.png", x=20, y=60)
+pdf.output("pdf-with-image.pdf")
