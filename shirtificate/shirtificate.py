909264e7b569e74647947c448843d34ce12ce8c6
leesishere <leesishere@gmail.com>
2025-02-09 19:09:03 +0000

    Sun, Feb 9, 2025, 2:09 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,7 +4,17 @@ _PATH = '/workspaces/21178063/shirtificate'
 image_path = f"{_PATH}/shirtificate.png"
 
 from fpdf import FPDF
-
+class PDF():
+    def _init_(self, name):
+        self._pdf = FPDF()
+        self._pdf. add_page()
+        self._pdf.set_font("helvetica", "B", 50)
+        self._pdf.cell(0, 60, 'CS50 Shirtificate', new_x="LMARGIN", new_y="NEXT", align='C*)
+        self._pdf.image("shirtificate.png", w=self._pdf.epw)
+        self._pdf.set_font_size(30)
+        self._pdf.set_text_color (255, 255, 255)
+        self._pdf.text(x=47.5, y=140, txt=f" (name) took CS50")
+'''
 class PDF(FPDF):
     def __init__(self, name, orientation='P', format='A4'):
         super().__init__(orientation=orientation, format=format)
@@ -22,9 +32,9 @@ class PDF(FPDF):
     @name.setter
     def name(self, myname):
         self._name = myname
-
-
-pdf = PDF(input("Name: "))
+'''
+name = input("Name: ")
+pdf = PDF(name)
 pdf.set_font_size(30)
 pdf.set_text_color(255, 255, 255)
 pdf.text(x=47.4,y=140, text=f"{pdf.name} took CS50")
@@ -33,3 +43,13 @@ pdf.text(x=47.4,y=140, text=f"{pdf.name} took CS50")
 # Output PDF
 pdf.output('shirtificate.pdf')
 
+class PDF():
+    def _init_(self, name):
+        self._pdf = FPDF()
+        self._pdf. add_page()
+        self._pdf.set_font("helvetica", "B", 50)
+        self._pdf.cell(0, 60, 'CS50 Shirtificate', new_x="LMARGIN", new_y="NEXT", align='C*)
+        self._pdf.image("shirtificate.png", w=self._pdf.epw)
+        self._pdf.set_font_size(30)
+        self._pdf.set_text_color (255, 255, 255)
+        self._pdf.text(x=47.5, y=140, txt=f" (name) took CS50")
