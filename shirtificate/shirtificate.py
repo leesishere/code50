e40d920811538bf0f1f28165db28bda02a273255
leesishere <leesishere@gmail.com>
2025-02-09 20:05:03 +0000

    Sun, Feb 9, 2025, 3:05 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -37,11 +37,11 @@ class PDF:
         self._pdf.set_font("helvetica", "B", 50)
         self._pdf.cell(0, 60, 'CS50 Shirtificate', new_x="LMARGIN", new_y="NEXT", align='C')
         self._pdf.image("shirtificate.png", w=self._pdf.epw)
-        self._pdf.set_font_size(30)
+        self._pdf.set_font_size(25)
         self._pdf.set_text_color(255, 255, 255)
 
     def write_message(self):
-        self._pdf.text(x=47.5, y=140, text=f"{self._pdf._name} took CS50")
+        self._pdf.text(x=54, y=140, text=f"{self._pdf._name} took CS50")
 
     def output(self, filename):
         self._pdf.output(filename)
