edc4b241d1373b9ec0d8f72de914c1b675555fb6
leesishere <leesishere@gmail.com>
2025-02-09 19:51:03 +0000

    Sun, Feb 9, 2025, 2:51 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,7 +4,7 @@ _PATH = '/workspaces/21178063/shirtificate'
 image_path = f"{_PATH}/shirtificate.png"
 
 from fpdf import FPDF
-
+'''
 class PDF(FPDF):
     def __init__(self, name, orientation='Portrait', format='A4'):
         super().__init__(orientation=orientation, format=format)
@@ -32,17 +32,20 @@ class PDF(FPDF):
 class PDF:
     def __init__(self, name):
         self._pdf = FPDF()
+        self._pdf = name
         self._pdf.add_page()
         self._pdf.set_font("helvetica", "B", 50)
         self._pdf.cell(0, 60, 'CS50 Shirtificate', new_x="LMARGIN", new_y="NEXT", align='C')
         self._pdf.image("shirtificate.png", w=self._pdf.epw)
         self._pdf.set_font_size(30)
         self._pdf.set_text_color(255, 255, 255)
+
+    def text(self, text):
         self._pdf.text(x=47.5, y=140, text=f"{name} took CS50")
 
     def output(self, filename):
         self._pdf.output(filename)
-'''
+
 name = input("Name: ")
 pdf = PDF(name)
 
