2489c76049f395498def1a4e9f55d4d661be3c94
leesishere <leesishere@gmail.com>
2025-02-10 11:56:35 +0000

    Mon, Feb 10, 2025, 6:56 AM -05:00

--- project/read.py
+++ project/read.py
@@ -1,20 +1,19 @@
 
-def load_dict_from_txt(filename):
-    with open(filename, 'r') as file:
-        data = file.read().strip()
-        # Convert string representation of dictionary to actual dictionary
-        word_dict = eval(data)
-    return word_dict
+import json
 
+def load_word_dictionary(filename):
+    with open(filename, 'r') as file:
+        word_list = json.load(file)
+    return {entry['word']: entry for entry in word_list}
 
 def search_word(word, word_dict):
     return word_dict.get(word, 'Word not found in the dictionary.')
 
 # Usage
 filename = 'word_dict.json'
-word_dict = load_dict_from_txt(filename)
+word_dict = load_word_dictionary(filename)
 
 # Example searches
-print(search_word('alibi', word_dict))
+print(search_word('ablative', word_dict))
 print(search_word('able', word_dict))
 print(search_word('Ache', word_dict))  # This word is not in the dictionary
