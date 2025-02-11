6f392c13e866f923a480a7f86be6eb8a371dc883
leesishere <leesishere@gmail.com>
2025-02-11 00:01:15 +0000

    Mon, Feb 10, 2025, 7:01 PM -05:00

--- project/work_complexity.py
+++ project/work_complexity.py
@@ -18,12 +18,19 @@ def get_word(word):
     else:
         return None
 
-def get_howtosay(data):
+def get_audio(data):
     try:
-        audio = data['phonetics'][0]['audio']
+        audio = data['phonetics'][1]['audio']
     except:
         audio = False
     return audio
+
+def get_text(data):
+    try:
+        text = data['phonetics'][1]['text']
+    except:
+        text = False
+    return text
 
 def get_meaning(data):
     try:
@@ -71,8 +78,9 @@ def has_morphological_complexity(word):
 def analyze_word_complexity(word, word_record):
     complexities = {
         'word': word,
-        'how_to_say': get_howtosay(word_record),
-        'meanings' : get_meaning(word_record)
+        'audio': get_audio(word_record),
+        'text': get_text(word_record)
+        'meanings':get_meaning(word_record)
         'letter_count': count_letters(word),
         'syllable_count': count_syllables(word),
         'phonetic_complexity': is_phonetically_complex(word),
@@ -88,8 +96,8 @@ def open_and_parse_file(filename,out_filename, cnt):
     with open(filename, 'r') as rfile:
         lines = rfile.readlines()
         for word in lines:
-            word_record = get_word(word.strip())
-            if how_to_say:
+            audio = get_word(word.strip())
+            if audio:
                 word_complexities.update(analyze_word_complexity(word.strip(),word_record))
                 append_to_json(word_complexities,out_filename)
                 if count > cnt:
