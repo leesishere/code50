74c23c694649b3b26b2761336ac2c0e80f9385b5
leesishere <leesishere@gmail.com>
2025-02-16 00:20:20 +0000

    Sat, Feb 15, 2025, 7:20 PM -05:00

--- project/project.py
+++ project/project.py
@@ -1,10 +1,18 @@
 def main():
-    ...
+    unction_1()
 
 
 def function_1():
-    ...
+    menu = "Welcome to the word guess Game \
 
+    Please select the game level \
+    1  Elementary \
+    2  High School \
+    3  Bachelor \
+    4 Master \
+    5  PhD \
+    "
+    print(menu)
 
 def function_2():
     ...
