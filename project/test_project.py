37b421625a59d5dd96a9726c2908e4b1b36a810b
leesishere <leesishere@gmail.com>
2025-03-16 18:12:33 +0000

    Sun, Mar 16, 2025, 2:12 PM -04:00

--- project/test_project.py
+++ project/test_project.py
@@ -203,10 +203,10 @@ def test_main():
             assert True, f"'High Scores:' was found in output"
             child.sendline("")
             child.sendline("5")
-            if re.search(r'Playing level 5', line)
-                assert True, f"'We made it to the game to start playing :-)"
+            if re.search(r'Playing level 5', line):
+                assert True, f"We made it to the game to start playing :-)"
             else:
-                assert False, f"'We did not make it to the game to start playing :-()"
+                assert False, f"We did not make it to the game to start playing :-()"
             # this kills the game when a user clicks the return key more that 5 times
             child.sendline("")
             child.sendline("")
