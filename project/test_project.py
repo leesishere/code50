61753b46d3f6a1ca4cd9bdf8ca8a6e6d571ca90f
leesishere <leesishere@gmail.com>
2025-03-16 17:55:43 +0000

    Sun, Mar 16, 2025, 1:55 PM -04:00

--- project/test_project.py
+++ project/test_project.py
@@ -197,7 +197,15 @@ def test_main():
     if output == 'Username?' and not any([exception in line for line in output.split('\n')]):
         assert output == 'Clear'
 
-    child.expect(re.compile(r"Press any key to continue"))
+    child.expect(re.compile(r"High Scores:"))
+    high_scores = get_output(child, re.compile(r'High Scores:'))
+    #assert len(high_scores) > 0
+
+    #child.expect(re.compile(r"Press any key to continue"))  # Match the continue prompt
+    #any_key = get_output(child, re.compile(r'Press any key to continue'))
+    #assert len(any_key) > 0
+    #child.sendline("")  # Pressing any key
+
     # this kills the game when a user clicks the return key more that 5 times
     child.sendline("")
     child.sendline("")
@@ -205,13 +213,3 @@ def test_main():
     child.sendline("")
     child.sendline("")
     child.sendline("")
-
-    high_scores = get_output(child, re.compile(r'Press any key to continue'))
-    assert len(high_scores) > 0
-
-    #child.expect(re.compile(r"Press any key to continue"))  # Match the continue prompt
-    #any_key = get_output(child, re.compile(r'Press any key to continue'))
-    #assert len(any_key) > 0
-    #child.sendline("")  # Pressing any key
-
-
