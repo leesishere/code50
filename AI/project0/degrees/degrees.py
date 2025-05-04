09b59ec8193bc69255a18cec7e5cc0ef03504c86
leesishere <leesishere@gmail.com>
2025-05-04 16:03:06 +0000

    Sun, May 4, 2025, 12:03 PM -04:00

--- AI/project0/degrees/degrees.py
+++ AI/project0/degrees/degrees.py
@@ -115,8 +115,9 @@ def shortest_path(source, target):
     # TODO
 
     for key, value in people.items():
-        print(f"{key}: {value.get('name', '')}, born: {value.get('birth', '')}, {len(value.get('movies', ''})
-        len(my_set)
+        print(f"{key}: {value.get('name', '')}, born: {value.get('birth', '')}, {len(value.get('movies', ''))})
+        
+
         #print(f"{key}: {value.get('name', '')}, born: {value.get('birth', '')}", end=', ')
         #for movie_id in value.get('movies', ''):
         #    print(f"{movie_id}", end=', ' )
