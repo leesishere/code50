a26ea156dc925917163516b45dbd7b73fcaa63a4
leesishere <leesishere@gmail.com>
2025-05-04 19:52:44 +0000

    Sun, May 4, 2025, 3:52 PM -04:00

--- AI/project0/degrees/degrees.py
+++ AI/project0/degrees/degrees.py
@@ -118,8 +118,9 @@ def shortest_path(source, target):
     # 'Tom Hanks' & 'Tom Cruise' approach
     for m in people[source]['movies']:
         for star in movies[m]['stars']:
-            
-            print(star, end=', ')
+            star_power = same_movie(target,star)
+            if star_power:
+                print("C is in the same movie as B")
     print()
 
 
