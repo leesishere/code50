ea1ba2a79db5d34f2c9f4006e73d95c8296ed6f3
leesishere <leesishere@gmail.com>
2025-05-04 19:50:46 +0000

    Sun, May 4, 2025, 3:50 PM -04:00

--- AI/project0/degrees/degrees.py
+++ AI/project0/degrees/degrees.py
@@ -118,6 +118,7 @@ def shortest_path(source, target):
     # 'Tom Hanks' & 'Tom Cruise' approach
     for m in people[source]['movies']:
         for star in movies[m]['stars']:
+            
             print(star, end=', ')
     print()
 
