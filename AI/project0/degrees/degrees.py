74c9cb76ce4827d26ab90888b08108b1177e8325
leesishere <leesishere@gmail.com>
2025-05-04 21:00:43 +0000

    Sun, May 4, 2025, 5:00 PM -04:00

--- AI/project0/degrees/degrees.py
+++ AI/project0/degrees/degrees.py
@@ -122,11 +122,11 @@ def shortest_path(source, target):
     print(people[source]['movies'])
     print(people[target]['movies'])
 
-   # for m in people[source]['movies']:
-   #     for star in movies[m]['stars']:
-   #         star_power = same_movie(star,target)
-   #         if star_power:
-   #             print(f"C is in the same movie as B - {star_power}")
+    for m in people[source]['movies']:
+        for star in movies[m]['stars']:
+            star_power = same_movie(star,target)
+            if star_power:
+                print(f"C is in the same movie as B - {star_power}")
     print()
 
 
