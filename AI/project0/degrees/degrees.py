4ca98cd1e814cfd3aaff04d7c90533d96a4d4cf6
leesishere <leesishere@gmail.com>
2025-05-10 18:25:08 +0000

    Sat, May 10, 2025, 2:25 PM -04:00

--- AI/project0/degrees/degrees.py
+++ AI/project0/degrees/degrees.py
@@ -174,20 +174,20 @@ def shortest_path(source, target):
     cursor.executemany("INSERT INTO actor_movies (degree, actor_id, movie_id) VALUES (:degree, :actor_id, :movie_id)", sorted_actor_data)
     conn.commit()
 
-    _sql  = "WITH RECURSIVE actor_paths AS ( "
-    _sql += "-- Base case: Start from actors with degree = 1 "
-    _sql += "SELECT actor_id, movie_id, degree, actor_id AS path "
-    _sql += "FROM actor_movies "
-    _sql += "WHERE degree = 1 "
-    _sql += " "
-    _sql += "UNION ALL "
-    _sql += " "
-    _sql += "-- Recursive case: Join on the same movie_id to find connections "
-    _sql += "SELECT a.actor_id, a.movie_id, a.degree, p.path || \' -> \' || a.actor_id "
-    _sql += "FROM actor_movies AS a "
-    _sql += "JOIN actor_paths AS p ON a.movie_id = p.movie_id "
-    _sql += "WHERE a.degree = -1 "
-    _sql += ")"
+    _sql  = "WITH RECURSIVE actor_paths AS ( \n"
+    _sql += "-- Base case: Start from actors with degree = 1 \n"
+    _sql += "SELECT actor_id, movie_id, degree, actor_id AS path \n"
+    _sql += "FROM actor_movies \n"
+    _sql += "WHERE degree = 1 \n"
+    _sql += " \n"
+    _sql += "UNION ALL \n"
+    _sql += " \n"
+    _sql += "-- Recursive case: Join on the same movie_id to find connections \n"
+    _sql += "SELECT a.actor_id, a.movie_id, a.degree, p.path || \' -> \' || a.actor_id \n"
+    _sql += "FROM actor_movies AS a \n"
+    _sql += "JOIN actor_paths AS p ON a.movie_id = p.movie_id \n"
+    _sql += "WHERE a.degree = -1 \n"
+    _sql += ") \n"
 
     cursor.execute(_sql)
     rows = cursor.fetchall()
