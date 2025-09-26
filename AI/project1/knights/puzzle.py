25961f4001d5033f1d09228e69bcdd39c1cf8d2c
Jamey <leesishere@gmail.com>
2025-09-26 17:00:53 -0400

    updated logic

--- AI/project1/knights/puzzle.py
+++ AI/project1/knights/puzzle.py
@@ -9,24 +9,46 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+common_knowledge = And(
+    # A is either a knight or a knave
+    Or(AKnight, AKnave),
+    # A cannot be both a knight and a knave
+    Not(And(AKnight, AKnave)),
+    # B is either a knight or a knave
+    Or(BKnight, BKnave),
+    # B cannot be both a knight and a knave
+    Not(And(BKnight, BKnave)),
+    # C is either a knight or a knave
+    Or(CKnight, CKnave),
+    # C cannot be both a knight and a knave
+    Not(And(CKnight, CKnave))
+)
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave))),
+    common_knowledge
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave))),
+    common_knowledge
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
+    common_knowledge
 )
 
 # Puzzle 3
@@ -35,7 +57,15 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+   Implication(AKnight, Or(AKnight, AKnave)),
+   Implication(AKnave, Not(Or(AKnight, AKnave))),
+   Implication(BKnight, AKnave),
+   Implication(BKnave, Not(AKnave)),
+   Implication(BKnight, CKnave),
+   Implication(BKnave, Not(CKnave)),
+   Implication(CKnight, AKnight),
+   Implication(CKnave, Not(AKnight)),
+   common_knowledge
 )
 
 
